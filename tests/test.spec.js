279fb5c2674b1fe24a7f43bba1972f75985f61cc
lg1382 <lg1382@hotmail.com>
2016-03-06 15:19:19 -0800

    Update test.spec.js

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,9 +1,5 @@
 describe('math', function() {
   it('should do math', function () {
-<<<<<<< HEAD
-    expect(1+1).toEqual(1);
-=======
     expect(1+1).toEqual(2);
->>>>>>> origin/master
   });
 });
